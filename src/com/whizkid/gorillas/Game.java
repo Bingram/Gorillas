c2831c7cd4c47166d28ecea2dcf53c2698397440
Bingram <ingram.brian@gmail.com>
2014-09-09 15:04:25 -0700

    Made some notes in the extended package for implementing more physics based collision and graphics.

--- src/com/whizkid/gorillas/Game.java
+++ src/com/whizkid/gorillas/Game.java
@@ -1,5 +1,17 @@
 package com.whizkid.gorillas;
 
 public abstract class Game {
+
 	
+	/**
+	 * CLASS NOTES
+	 * 
+	 * http://gamedevelopment.tutsplus.com/tutorials/how-to-create-a-custom-2d-physics-engine-the-basics-and-impulse-resolution--gamedev-6331
+	 * 
+	 * http://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-likely-collisions-in-2d-space--gamedev-374
+	 * 
+	 * http://gamedevelopment.tutsplus.com/tutorials/make-your-game-pop-with-particle-effects-and-quadtrees--gamedev-2138
+	 * 
+	 * 
+	 */
 }
